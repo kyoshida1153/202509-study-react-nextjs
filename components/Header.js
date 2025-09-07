1128583139bdac3c3fb2d60133c530f88b34941d
Yoshida.Kazutaka <kyoshida1153@gmail.com>
2025-09-07 17:50:22 +0900

    ✨ ヘッダーの追加

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import styles from "./Header.module.css";
+
+import Link from "next/link";
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <Link href="/">Index</Link>
+      <Link href="/about">About</Link>
+    </header>
+  );
+}
