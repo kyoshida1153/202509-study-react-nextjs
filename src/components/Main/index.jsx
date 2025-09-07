bcd707d2770b7a694ecd12723cbbbd9f7e87f27a
Yoshida.Kazutaka <kyoshida1153@gmail.com>
2025-09-07 19:39:18 +0900

    ✨ カウンターを設置してuseStateでカウントアップする

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,8 +1,18 @@
 import styles from "@/components/Main/Main.module.css";
 import { Headline } from "@/components/Headline";
 import { Links } from "@/components/Links";
+import { useEffect } from "react";
 
 export function Main(props) {
+  useEffect(() => {
+    console.log("マウント時");
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+      console.log("アンマウント時");
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
+
   return (
     <main className={styles.main}>
       <Headline page={props.page}>
