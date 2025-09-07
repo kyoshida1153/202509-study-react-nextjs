79f361538691846d86b7e978858f91ae8197ab4a
Yoshida.Kazutaka <kyoshida1153@gmail.com>
2025-09-07 23:32:02 +0900

    ♻️ リファクタリング

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,20 @@
 import styles from "@/components/Header/Header.module.css";
-
 import Link from "next/link";
 
+const NAV_ITEMS = [
+  { href: "/", label: "Index" },
+  { href: "/about", label: "About" },
+];
 export function Header() {
   return (
     <header className={styles.header}>
-      <Link href="/">Index</Link>
-      <Link href="/about">About</Link>
+      {NAV_ITEMS.map((item) => {
+        return (
+          <Link key={item.href} href={item.href}>
+            {item.label}
+          </Link>
+        );
+      })}
     </header>
   );
 }
